8ad168f48ab1ee83d506a4a7cebc086a22930b9b
Roberto Huertas <roberto.huertas@outlook.com>
2018-07-31 02:30:51 +0200

    more idiomatic ts

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,6 @@
-// async function init() {
-//   const mod = await import('url-checker');
-//   const result = mod.isUrl('http://terra.es');
-//   console.log('The string is a url: ', result);
-// }
+async function init() {
+  const { isUrl } = await import('url-checker');
 
-// init();
-
-const mod = import('url-checker');
-mod.then(({ isUrl }) => {
   const input = document.querySelector('#text')! as HTMLInputElement;
   const button = document.querySelector('#button')! as HTMLButtonElement;
   const result = document.querySelector('#result')!;
@@ -18,4 +11,6 @@ mod.then(({ isUrl }) => {
       isUrl(url) ? '' : 'not '
     }a valid url.`;
   });
-});
+}
+
+init();
