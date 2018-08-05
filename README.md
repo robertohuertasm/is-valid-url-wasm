40e9810689a3ab3f10b90fb25cd766d6f3aa137a
Roberto Huertas <roberto.huertas@outlook.com>
2018-08-06 00:32:19 +0200

    fixed minor issues

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ For the moment, given that `wasm-pack` only offers either support for the browse
 
 Note also that `WASM` loads asynchronously so the import has to be `async` too.
 
+The package has been created using:
+
+`wasm-pack init -m no-install`
+
 ## Example
 
 If you want to run the example, access to the `example` folder and run `npm i`. Then run `npm run serve` to start the `webpack` server.
@@ -14,5 +18,5 @@ Note that this example is using `TypeScript`.
 
 ## Improvements
 
-1.  Try to provide a package that can be used both in `NodeJs` and in the `browser`.
-1.  Reduce the size of the `.wasm` file by using [binaryen](https://github.com/WebAssembly/binaryen)
+1. Try to provide a package that can be used both in `NodeJs` and in the `browser`.
+1. Reduce the size of the `.wasm` file by using [binaryen](https://github.com/WebAssembly/binaryen)
--- pkg/url_checker.js
+++ pkg/url_checker.js
@@ -1,44 +1,46 @@
 /* tslint:disable */
-import * as wasm from './url_checker_bg.wasm';
+import * as wasm from './url_checker_bg';
 
 let cachedEncoder = new TextEncoder('utf-8');
 
 let cachegetUint8Memory = null;
 function getUint8Memory() {
-  if (
-    cachegetUint8Memory === null ||
-    cachegetUint8Memory.buffer !== wasm.memory.buffer
-  ) {
-    cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
-  }
-  return cachegetUint8Memory;
+    if (cachegetUint8Memory === null || cachegetUint8Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory;
 }
 
 function passStringToWasm(arg) {
-  const buf = cachedEncoder.encode(arg);
-  const ptr = wasm.__wbindgen_malloc(buf.length);
-  getUint8Memory().set(buf, ptr);
-  return [ptr, buf.length];
+    
+    const buf = cachedEncoder.encode(arg);
+    const ptr = wasm.__wbindgen_malloc(buf.length);
+    getUint8Memory().set(buf, ptr);
+    return [ptr, buf.length];
 }
 /**
- * @param {string} arg0
- * @returns {boolean}
- */
+* @param {string} arg0
+* @returns {boolean}
+*/
 export function isUrl(arg0) {
-  const [ptr0, len0] = passStringToWasm(arg0);
-  try {
-    return wasm.isUrl(ptr0, len0) !== 0;
-  } finally {
-    wasm.__wbindgen_free(ptr0, len0 * 1);
-  }
+    const [ptr0, len0] = passStringToWasm(arg0);
+    try {
+        return (wasm.isUrl(ptr0, len0)) !== 0;
+        
+    } finally {
+        wasm.__wbindgen_free(ptr0, len0 * 1);
+        
+    }
+    
 }
 
 let cachedDecoder = new TextDecoder('utf-8');
 
 function getStringFromWasm(ptr, len) {
-  return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+    return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
 }
 
 export function __wbindgen_throw(ptr, len) {
-  throw new Error(getStringFromWasm(ptr, len));
+    throw new Error(getStringFromWasm(ptr, len));
 }
+
